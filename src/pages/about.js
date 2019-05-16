7ab323184685f6b4952dc365336302e51baa0900
callelacouture <clacouture@klru.org>
2019-05-16 11:49:48 -0500

    link to home

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby";
 import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
     <Header headerText="About Gatsby"/>
     <p>Such wow. Very React.</p>
+    <Link to="/">Go Home</Link>
   </div>
 )
