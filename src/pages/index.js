d7761824a6d059bdc3c6285ed2593268648e2eb2
callelacouture <clacouture@klru.org>
2019-05-16 11:17:07 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import {Link} from 'gatsby'
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => <div style={{ color: `green`}}>
+<Header headerText="This is the Home Page"/>
+  <h1>Howdy World!</h1>
+  <p>This is a paragraph.</p>
+  <Link to="/about">About</Link>
+  <br></br>
+  <img src='https://source.unsplash.com/random/600x300' alt='imageRandom'></img>
+</div>
